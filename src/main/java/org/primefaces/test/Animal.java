3501d72b379aac87bce475f55f20b832368a26b4
Steve Hallworth-Cook <steve.hallworth-cook@ukaea.uk>
2021-11-22 15:38:29 +0000

    PF11RC2 AUTOCOMPLETE READONLY ANOMALY

---
+++ src/main/java/org/primefaces/test/Animal.java
@@ -0,0 +1,42 @@
+package org.primefaces.test;
+
+import java.util.List;
+import java.util.Map;
+
+public class Animal {
+
+    private String type;
+    private String name;
+    private String color;
+
+    public Animal(String type, String name, String color) {
+        this.type = type;
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -3,6 +3,9 @@ package org.primefaces.test;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -13,15 +16,35 @@ import lombok.Data;
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
+
     private String string;
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
-    
-    @PostConstruct  
+
+    private List<Animal> animalList;
+
+    @PostConstruct
     public void init() {
-        string = "Welcome to PrimeFaces!!!";
+        string = "Welcome to PrimeFaces!!";
+
+        animalList = Arrays.asList(
+                new Animal("Tiger", "Max", "White"),
+                new Animal("Elephant", "Nellie", "Pink"),
+                new Animal("Bear", "Balloo", "Brown")
+        );
+    }
+
+    public List<Animal> acAnimal() {
+        return new ArrayList<Animal>();
+    }
+
+    public List<Animal> getAnimalList() {
+        return animalList;
+    }
+
+    public void setAnimalList(List<Animal> animalList) {
+        this.animalList = animalList;
     }
 
 }
